94a1f9ab05963d971f40ed56054d1f8037dde028
Olya27 <74761167+Olya27@users.noreply.github.com>
2021-03-31 03:54:58 +0300

    Add files via upload

---
+++ if const/if const/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace if_const
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = new int[] {1, 2, 1, 10, 9 };
+            object p = x;
+            Array.Sort(x);
+            int i = 0;
+            bool r;
+            int k = 0;
+            while (i < x.Length-1)
+            {
+                k = x[i + 1] - x[i] <= 0 ? k+=1 : k;
+                i++;
+            }
+            if (k > 0) r = false;
+            else r = true;
+            Console.WriteLine(r);
+            
+        }
+    }
+}
